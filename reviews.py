9333bf843f362ba3f04e12c2f660fd62288960a7
Ivy <ivyownn@gmail.com>
2023-12-20 20:50:53 -0500

    new wine

--- reviews.py
+++ reviews.py
@@ -5,8 +5,6 @@ import pandas as pd
 reviews = pd.read_csv("/content/sample_data/winemag-data-130k-v2.csv")
 
 
-#reviews = pd.read_csv("Users/aivee/Desktop/Code_You/DataAnalysis/wine-reviews-exercise-ivyownn/data/winemag-data-130k-v2.csv", index_col=0)
-
 reviews.points.describe()
 
 #median_points = reviews.points.unique()
